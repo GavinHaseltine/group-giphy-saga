e35fc00b36a0b1e73b93083dbd7a3cc4377f4ce5
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-08-24 16:08:31 -0500

    got the POST,GET,PUT working

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,18 +5,43 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  const  queryText = 'SELECT * FROM "favorites";'
+  pool.query(queryText)
+    .then((results) => {
+      res.send(results.rows)
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    })
 });
 
 // add a new favorite
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  const queryText = `INSERT INTO "favorites" ("url")
+  VALUES ($1);`
+  const queryParams = [req.body.name]
+  pool.query(queryText,queryParams)
+      .then((result) => {
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        res.sendStatus(500);
+      })
 });
 
 // update given favorite with a category id
 router.put('/:favId', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+let category_id = req.body.category_id
+let favID = req.params.favId
+const queryText = `UPDATE "favorites" SET category_id = $1 WHERE id = $2;`
+const queryParams = [category_id, favID]
+pool.query(queryText,queryParams)
+    .then((results) => {
+      res.sendStatus(200);
+    }).catch((error) => {
+      res.sendStatus(500);
+    })
 });
 
 // delete a favorite

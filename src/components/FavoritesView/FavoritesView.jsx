3ac4ebeef353763e100fba0479aa9ebc8158bfdd
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-08-24 17:24:13 -0500

    working on mapping over

--- src/components/FavoritesView/FavoritesView.jsx
+++ src/components/FavoritesView/FavoritesView.jsx
@@ -1,20 +1,31 @@
 import axios from 'axios';
+import {useState} from 'react'
 
 function FavoritesView() {
+
+    const[newFavorite, setNewFavorite] = useState('')
 
     const getFavorite = () => {
     axios.get('/api/favorite')
         .then((response) => {
-            console.log('response:', response)
+            console.log('response:', response.data)
+            setNewFavorite(response.data);
         }).catch((error) => {
             console.log('error with the GET favorite', error)
         })
     }
+    getFavorite();
 
     return (
         <>
             <h1>Favorites View</h1>
-            
+            <ul> 
+                {newFavorite.map(favorite => 
+                <li key={ favorite.id }> 
+                    {favorite.url}
+                </li>
+                )}
+            </ul>
 
 
         </>

d7830bc22fa6316cf374bd2a0ca6dca77adfc751
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-08-24 21:12:55 -0500

    finsihed up the map

--- src/components/FavoritesView/FavoritesView.jsx
+++ src/components/FavoritesView/FavoritesView.jsx
@@ -1,29 +1,38 @@
 import axios from 'axios';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+
 
 function FavoritesView() {
 
-    const[newFavorite, setNewFavorite] = useState('')
+    const[newFavorite, setNewFavorite] = useState([])
 
     const getFavorite = () => {
     axios.get('/api/favorite')
         .then((response) => {
             console.log('response:', response.data)
             setNewFavorite(response.data);
+            
         }).catch((error) => {
             console.log('error with the GET favorite', error)
         })
     }
-    getFavorite();
+
+    useEffect( () => {
+        getFavorite();
+    }, [])
+   
 
     return (
         <>
             <h1>Favorites View</h1>
+        
             <ul> 
                 {newFavorite.map(favorite => 
+                //the if codition for the drop box goes here 
                 <li key={ favorite.id }> 
                     {favorite.url}
                 </li>
+                //Drop down goes here 
                 )}
             </ul>
 
